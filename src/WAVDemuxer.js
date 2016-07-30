1fe58b5d99a7cb36946b2deb080a8a580f4d62d4
Devon Govett <devongovett@gmail.com>
2016-07-29 21:54:19 -0700

    Refactor wav demuxer

--- src/WAVDemuxer.js
+++ src/WAVDemuxer.js
@@ -1,6 +1,7 @@
 import Demuxer from './Demuxer';
+import Track from './Track';
 
-let formats = { 
+const formats = { 
   0x0001: 'lpcm',
   0x0003: 'lpcm',
   0x0006: 'alaw',
@@ -14,78 +15,64 @@ export default class WAVDemuxer extends Demuxer {
   }
                 
   readChunk() {
-    if (!this.readStart && this.stream.available(12)) {
-      if (this.stream.readString(4) !== 'RIFF') {
-        return this.emit('error', 'Invalid WAV file.');
-      }
-        
-      this.fileSize = this.stream.readUInt32(true);
-      this.readStart = true;
-      
-      if (this.stream.readString(4) !== 'WAVE') {
-        return this.emit('error', 'Invalid WAV file.');
-      }
+    if (!this.type) {
+      this.type = this.stream.readString(4);
+      this.len = this.stream.readUInt32(true); // little endian
     }
     
-    while (this.stream.available(1)) {
-      if (!this.readHeaders && this.stream.available(8)) {
-        this.type = this.stream.readString(4);
-        this.len = this.stream.readUInt32(true); // little endian
-      }
+    switch (this.type) {
+      case 'RIFF':
+        if (this.stream.readString(4) !== 'WAVE') {
+          throw new Error('Invalid WAV file.');
+        }
         
-      switch (this.type) {
-        case 'fmt ':
-          let encoding = this.stream.readUInt16(true);
-          if (!(encoding in formats)) {
-            return this.emit('error', 'Unsupported format in WAV file.');
-          }
-            
-          this.format = {
-            formatID: formats[encoding],
-            floatingPoint: encoding === 0x0003,
-            littleEndian: formats[encoding] === 'lpcm',
-            channelsPerFrame: this.stream.readUInt16(true),
-            sampleRate: this.stream.readUInt32(true),
-            framesPerPacket: 1
-          };
-            
-          this.stream.advance(4); // bytes/sec.
-          this.stream.advance(2); // block align
+        break;
+        
+      case 'fmt ':
+        let encoding = this.stream.readUInt16(true);
+        if (!(encoding in formats)) {
+          throw new Error('Unsupported format in WAV file.');
+        }
           
-          this.format.bitsPerChannel = this.stream.readUInt16(true);
-          this.format.bytesPerPacket = (this.format.bitsPerChannel / 8) * this.format.channelsPerFrame;
+        this.format = {
+          formatID: formats[encoding],
+          floatingPoint: encoding === 0x0003,
+          littleEndian: formats[encoding] === 'lpcm',
+          channelsPerFrame: this.stream.readUInt16(true),
+          sampleRate: this.stream.readUInt32(true),
+          framesPerPacket: 1
+        };
           
-          // Advance to the next chunk
-          this.stream.advance(this.len - 16);
-          break;
-          
-        case 'data':
-          if (this.tracks.length === 0) {
-            let bytes = this.format.bitsPerChannel / 8;
-            let duration = this.len / bytes / this.format.channelsPerFrame / this.format.sampleRate * 1000 | 0;
-            this.addTrack('audio', this.format, duration);
-          }
-          
-          let buffer = this.stream.readSingleBuffer(this.len);
-          this.len -= buffer.length;
-          this.readHeaders = this.len > 0;
-          this.tracks[0].write(new Buffer(buffer));
-          
-          if (this.len <= 0)
-            this.tracks[0].end();
-          
-          break;
-          
-        default:
-          if (!this.stream.available(this.len)) { return; }
-          this.stream.advance(this.len);
-      }
-            
-      if (this.type !== 'data') { this.readHeaders = false; }
+        this.stream.advance(4); // bytes/sec.
+        this.stream.advance(2); // block align
+        
+        this.format.bitsPerChannel = this.stream.readUInt16(true);
+        this.format.bytesPerPacket = (this.format.bitsPerChannel / 8) * this.format.channelsPerFrame;
+        
+        // Advance to the next chunk
+        this.stream.advance(this.len - 16);
+        break;
+        
+      case 'data':
+        if (this.tracks.length === 0) {
+          let bytes = this.format.bitsPerChannel / 8;
+          let duration = this.len / bytes / this.format.channelsPerFrame / this.format.sampleRate * 1000 | 0;
+          this.addTrack(new Track(Track.AUDIO, this.format, duration));
+        }
+        
+        let buffer = this.stream.readSingleBuffer(this.len);
+        this.len -= buffer.length;
+        this.tracks[0].write(buffer);
+        break;
+        
+      default:
+        this.stream.advance(this.len);
     }
-      
-    return;
+          
+    if (this.type !== 'data' || this.len === 0) {
+      this.type = null;
+    }
   }
 }
-    
-Demuxer.register(WAVEDemuxer);
+
+Demuxer.register(WAVDemuxer);
